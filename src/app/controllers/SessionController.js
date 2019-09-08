f0699a1efde281085d95b75f5badaa61f8afdbac
Jheison Maciel Ines <jheisonmaciel@hotmail.com>
2019-09-08 14:10:25 -0300

    Autenticação/Validação[User, Session]

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,14 +1,26 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      user_name: Yup.string()
+        .required('Informe nome de usuário!')
+        .min(4, 'O nome deve conter mais de 4 letras!'),
+      password: Yup.string().required('Informe a senha!'),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos!' });
+    }
+
     const { user_name, password } = req.body;
 
     const user = await User.findOne({ where: { user_name } });
     if (!user) {
-      return res.status(400).json({ error: 'Nenhum usuário encontrado!' });
+      return res.status(400).json({ error: 'Usuário não cadastrado!' });
     }
 
     if (!(await user.checkPassword(password))) {
@@ -22,8 +34,8 @@ class SessionController {
         user_name,
         email,
       },
-      token: jwt.sign({ id }, '6e6b389d9fa8a432c10ce901606fe30a', {
-        expiresIn: '30d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expired,
       }),
     });
   }
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,28 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
+  // Create Usuario ############################
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .required('Informe o nome!')
+        .min(3, 'O nome deve conter mais de 3 letras!'),
+      user_name: Yup.string()
+        .required('Informe nome de usuário!')
+        .min(4, 'O nome deve conter mais de 4 letras!'),
+      email: Yup.string()
+        .required('Informe email!')
+        .email('Informe email válido!'),
+      password: Yup.string()
+        .required('Informe a senha!')
+        .min(6, 'Informe senha com no mínimo 6 caracteres')
+        .max(8, 'Informe senha com no máximo 8 caracteres'),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos!' });
+    }
     // Controle de usuario já cadastrado
     const emailUserExists = await User.findOne({
       where: { email: req.body.email },
@@ -19,7 +40,7 @@ class UserController {
         .status(400)
         .json({ error: 'Nome de usuario já esta sendo usado!' });
     }
-    // ------------------------
+
     // Criando usuario
     const { id, user_name, email, account_status } = await User.create(
       req.body
@@ -31,6 +52,67 @@ class UserController {
       account_status,
     });
   }
+
+  // Create Usuario ############################
+  // ---------------------------------------------------------------------------
+  // Update de Usuario ###########################
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().min(3, 'O nome deve conter mais de 3 letras!'),
+      user_name: Yup.string().min(4, 'O nome deve conter mais de 4 letras!'),
+      email: Yup.string().email('Informe email válido!'),
+      oldPassword: Yup.string().min(6, 'Informe no mínimo 6 caracteres'),
+      password: Yup.string()
+        .min(6, 'Informe senha com no mínimo 6 caracteres')
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados inválidos!' });
+    }
+
+    const { user_name, email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+    if (email !== user.email) {
+      const emailUserExists = await User.findOne({
+        where: { email },
+      });
+      if (emailUserExists) {
+        return res
+          .status(400)
+          .json({ error: 'Este e-mail já esta sendo usado por uma conta!' });
+      }
+    }
+
+    if (user_name !== user.user_name) {
+      const userExists = await User.findOne({
+        where: { user_name },
+      });
+      if (userExists) {
+        return res.status(400).json({ error: 'Nome de usuário indisponível!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ erro: 'Senha inválida!' });
+    }
+
+    const { id, name, account_status } = await user.update(req.body);
+    return res.json({
+      id,
+      name,
+      user_name,
+      email,
+      account_status,
+    });
+  }
+  // Update de Usuario ###########################
 }
 
 export default new UserController();
